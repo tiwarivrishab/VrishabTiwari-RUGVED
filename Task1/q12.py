cbf86f7d0d67724534be37129333608530edbf71
tiwarivrishab <vrishabtiwari2007@gmail.com>
2025-09-30 18:18:50 +0530

    Add files via upload

--- Task1/q12.py
+++ Task1/q12.py
@@ -1,7 +1,30 @@
-n=int(input("Enter number: "))
-for i in range (0, n, +1):
-    for k in range (n-i-1, -1, -1):
+n = int(input("Enter number: "))
+
+# Upper half
+for i in range(0, n):  
+    for k in range(n-i-1, -1, -1):  
         print(" ", end="")
-    for j in range (0, i+1, +1):
-        print("*", end=" ")
-    print("\n")
+    for j in range(0, i+1): 
+        print("* ", end="")
+    print()
+
+# Lower half
+for i in range(n-1, -1, -1): 
+    for k in range(0, n-i):  
+        print(" ", end="")
+    for j in range(i+1, 0, -1): 
+        print("* ", end="")
+    print()
+  
+
+#n=4, code will run from 0 to 3
+
+#      *        =3
+#     * *       =2
+#    * * *      =1
+#   * * * *
+
+
+#n=3, code will run from 0 to 2
+#In order to reverse we gotta run it from 2 to 0
+
